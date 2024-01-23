d0f98c30b3ea93c6853f0014f5031300187d7c13
TomislavBratic <ticbra1337@gmail.com>
2024-01-23 21:45:46 +0100

    Add RouterLink Home

--- BanakApp/src/app/app.routes.ts
+++ BanakApp/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FamilyfarmComponent } from './components/familyfarm/familyfarm.component';
 import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    { path: 'home', component: AppComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'familyfarms', component: FamilyfarmComponent },
     { path: 'about', component: AboutComponent },
 ];
