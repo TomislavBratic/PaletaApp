4061b1752fc70ff994036f5e86c15d58cbd2f431
TomislavBratic <ticbra1337@gmail.com>
2024-02-07 18:22:51 +0100

    Added correct backend

---
+++ Backend/PaletaApp/Models/Blog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BanakDataAccess.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string Content { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public  bool status { get; set; }
+        [Required]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        public int LikeCount { get; set; }
+
+    }
+}
---
+++ Backend/PaletaApp/Models/DTO/CreatePersonRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+namespace BanakDataAccess.Models.DTO
+{
+    class CreatePersonRequest
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+        public List<Address> Addresses { get; set; } = new List<Address>();
+        public List<Email> Emails { get; set; } = new List<Email>();
+        public List<Blog> Blogs { get; set; } = new List<Blog>();
+    }
+}
