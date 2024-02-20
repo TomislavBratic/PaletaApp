b20b7daabcfe02d2f1e14a60e58778588926b755
TomislavBratic <ticbra1337@gmail.com>
2024-02-20 21:01:13 +0100

    Add registration component

--- PaletaApp/src/app/app.routes.ts
+++ PaletaApp/src/app/app.routes.ts
@@ -3,9 +3,12 @@ import { AppComponent } from './app.component';
 import { FamilyfarmComponent } from './components/familyfarm/familyfarm.component';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'familyfarms', component: FamilyfarmComponent },
     { path: 'about', component: AboutComponent },
+    {path: 'registration', component:RegistrationComponent}
 ];
